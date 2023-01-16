0153695e5f874ab0845818fa48823e44c45582a8
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-01-16 23:36:15 +0100

    Major changes

--- LSM_StreamlitApp.py
+++ LSM_StreamlitApp.py
@@ -53,45 +53,7 @@ from botocore.exceptions import ClientError
 
 from PASSWORDS import *
 
-#######################################################################
-
-# Functions to get size of file transferred to S3 buckets.
-
-# Taken from https://stackoverflow.com/questions/55799556/make-boto3-upload-calls-blocking-single-threaded
-
-def get_bufsize(buf, chunk = 1024) -> int:
-	start = buf.tell()
-	try:
-		size = 0 
-		while True: 
-			out = buf.read(chunk) 
-			if out: 
-				size += chunk 
-			else: 
-				break
-		return size
-	finally:
-		buf.seek(start)
-
-class ProgressPercentage(object):
-
-	def __init__(self, filename, buf):
-		self._filename = filename
-		self._size = float(get_bufsize(buf))
-		self._seen_so_far = 0
-		self._lock = threading.Lock()
-		self.start = None
-
-	def __call__(self, bytes_amount):
-		with self._lock:
-			if not self.start:
-				self.start = time.monotonic()
-			self._seen_so_far += bytes_amount
-			percentage = (self._seen_so_far / self._size) * 100
-
-			sys.stdout.write("\r %s currently %.2f%% done \n" % (self._filename, percentage))
-
-			sys.stdout.flush()
+from ProgressPercentageCalculator import *
 
 #######################################################################
 
@@ -353,7 +315,7 @@ with st.form(key = 'LSM_SCAN_FORM_KEY', clear_on_submit = True):
 		IlluminationLeftKey = st.session_state['-IlluminationLeftKey-']
 		IlluminationRightKey = st.session_state['-IlluminationRightKey-']
 
-		if (IlluminationLeftKey == False or IlluminationRightKey == False):
+		if (IlluminationLeftKey == False and IlluminationRightKey == False):
 			st.error('Select at least one option for the illuminations', icon = None)
 			st.stop()
 
@@ -452,9 +414,9 @@ with st.form(key = 'LSM_SCAN_FORM_KEY', clear_on_submit = True):
 
 				try:
 
-					# response = gwdg_client.upload_fileobj(SingleUploadedFile, bucket_name, amazon_bucket_target_name, Callback = ProgressPercentage(SingleUploadedFile.name, SingleUploadedFile))
+					response = gwdg_client.upload_fileobj(SingleUploadedFile, bucket_name, amazon_bucket_target_name, Callback = ProgressPercentage(SingleUploadedFile.name, SingleUploadedFile))
 
-					response = gwdg_client.upload_fileobj(SingleUploadedFile, bucket_name, amazon_bucket_target_name)
+# 					response = gwdg_client.upload_fileobj(SingleUploadedFile, bucket_name, amazon_bucket_target_name)
 
 				except ClientError as e:
 
--- RunStreamlitApp.py
+++ RunStreamlitApp.py
@@ -1,31 +1,150 @@
 import os
 import urllib.request
 import subprocess
+import packaging.version
+
+#############################################################################
+
+os.system('cls||clear')
+
+#############################################################################
+
+UMG_PROXY = 'http://anonymous@astaro01-proxy.med.uni-goettingen.de:8080'
+
+#############################################################################
 
 # get the current directory path
 directory_path = os.getcwd()
 
+#############################################################################
+
 # specify the file name
 file_name = "LSM_StreamlitApp.py"
+
+#############################################################################
 
 # specify the url of the file you want to download
 url = "https://raw.githubusercontent.com/ajinkya-kulkarni/MPINAT-LSM-Application/main/LSM_StreamlitApp.py"
 
-# download the file and save it to a local file
+#############################################################################
+
+# check if the file already exists in the current directory and delete it before downloading it again
+if os.path.exists(file_name):
+	os.remove(file_name)
+
+#############################################################################
+
+os.system("echo ''")
+
+#############################################################################
+
+# download the "LSM_StreamlitApp.py" file, first without a proxy and then with a proxy
 try:
+    # download the file and save it to a local file
     urllib.request.urlretrieve(url, file_name)
-    print(f"{file_name} downloaded successfully")
+    print(f"Latest {file_name} fetched successfully")
+
 except urllib.error.HTTPError as e:
-    print(f"Error: {e.code}")
-except urllib.error.URLError as e:
-    print(f"Error: {e.reason}")
+    print(f"HTTP Error: {e.code} {e.reason}")
+    try:
+        # specify the proxy
+        proxy_support = urllib.request.ProxyHandler({'http': UMG_PROXY})
+        opener = urllib.request.build_opener(proxy_support)
+        urllib.request.install_opener(opener)
+        # download the file and save it to a local file
+        urllib.request.urlretrieve(url, file_name)
+        print(f"Latest {file_name} fetched successfully")
+    except:
+        urllib.request.install_opener(None)
+        print("Failed to fetch the file with proxy and without proxy")
+except:
+    print("Failed to fetch the file without proxy")
+
+#############################################################################
+
+os.system("echo ''")
 
 # specify the path to the Streamlit app
 app_path = os.path.join(directory_path, file_name)
 
+#############################################################################
+
+# Check if streamlit >= 1.17.0, if not download latest version
+try:
+	current_version = subprocess.check_output(["pip", "show", "streamlit"]).decode("utf-8").split("\n")
+	current_version = [line.split(":")[1].strip() for line in current_version if "Version" in line][0]
+	latest_version = subprocess.check_output(["pip", "show", "-v", "streamlit"]).decode("utf-8").split("\n")
+	latest_version = [line.split(":")[1].strip() for line in latest_version if "Version" in line][0]
+	if packaging.version.parse(current_version) < packaging.version.parse("1.17.0"):
+		subprocess.run(["pip", "install", "--upgrade", "streamlit"], check=True)
+		print("streamlit package installed successfully")
+	else:
+		print("streamlit package version is already >= 1.17.0")
+except subprocess.CalledProcessError as e:
+	try:
+		subprocess.run(["pip", "install", "--upgrade", "--proxy", UMG_PROXY, "streamlit"], check=True)
+		print("streamlit package installed successfully")
+	except subprocess.CalledProcessError as e:
+		print("Error: Failed to install streamlit package")
+		print("Error code: ", e.returncode)
+		print("Error message: ", e.output)
+
+# Check if boto3 > 1.26.50, if not download latest version
+try:
+	current_version = subprocess.check_output(["pip", "show", "boto3"]).decode("utf-8").split("\n")
+	current_version = [line.split(":")[1].strip() for line in current_version if "Version" in line][0]
+	latest_version = subprocess.check_output(["pip", "show", "-v", "boto3"]).decode("utf-8").split("\n")
+	latest_version = [line.split(":")[1].strip() for line in latest_version if "Version" in line][0]
+	if packaging.version.parse(current_version) < packaging.version.parse("1.26.50"):
+		subprocess.run(["pip", "install", "--upgrade", "boto3"], check=True)
+		print("boto3 package installed successfully")
+	else:
+		print("boto3 package version is already >= 1.26.50")
+except subprocess.CalledProcessError as e:
+	try:
+		subprocess.run(["pip", "install", "--upgrade", "--proxy", UMG_PROXY, "boto3"], check=True)
+		print("boto3 package installed successfully")
+	except subprocess.CalledProcessError as e:
+		print("Error: Failed to install boto3 package")
+		print("Error code: ", e.returncode)
+		print("Error message: ", e.output)
+
+# Check if caosdb >= 0.10.0, if not download latest version
+try:
+	current_version = subprocess.check_output(["pip", "show", "caosdb"]).decode("utf-8").split("\n")
+	current_version = [line.split(":")[1].strip() for line in current_version if "Version" in line][0]
+	latest_version = subprocess.check_output(["pip", "show", "-v", "caosdb"]).decode("utf-8").split("\n")
+	latest_version = [line.split(":")[1].strip() for line in latest_version if "Version" in line][0]
+	if packaging.version.parse(current_version) < packaging.version.parse("0.10.0"):
+		subprocess.run(["pip", "install", "--upgrade", "caosdb"], check=True)
+		print("caosdb package installed successfully")
+	else:
+		print("caosdb package version is already >= 0.10.0")
+except subprocess.CalledProcessError as e:
+	try:
+		subprocess.run(["pip", "install", "--upgrade", "--proxy", UMG_PROXY, "caosdb"], check=True)
+		print("caosdb package installed successfully")
+	except subprocess.CalledProcessError as e:
+		print("Error: Failed to install caosdb package")
+		print("Error code: ", e.returncode)
+		print("Error message: ", e.output)
+
+#############################################################################
+
+os.system("echo ''")
+
+#############################################################################
+
+os.system("echo 'Starting streamlit now...'")
+
+os.system("echo ''")
+
 # run the Streamlit app with the --server.maxUploadSize flag
 # 5000 = 5GB
-result = subprocess.run(["streamlit", "run", app_path, "--server.maxUploadSize=5000"])
+subprocess.run(["streamlit", "run", app_path, "--server.maxUploadSize=5000"])
 
-if result.returncode != 0:
-    print("An error occurred while running the Streamlit app.")
+#############################################################################
+
+# @echo off
+# "C:\path\to\python.exe" "C:\path\to\myscript.py"
+# pause
