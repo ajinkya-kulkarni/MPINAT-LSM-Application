ececde7c1fc009c3eb503d04788dd5f0e3d17d42
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-03-07 14:14:42 +0100

    Cleaned repo

--- LSM_StreamlitApp.py
+++ LSM_StreamlitApp.py
@@ -583,14 +583,6 @@ with st.form(key = 'LSM_SCAN_FORM_KEY', clear_on_submit = True):
 
 			if os.path.exists(file_path):
 				os.remove(file_path)
-	
-		file_names = ["uploaded_files_to_S3.txt"]
-
-		for file_name in file_names:
-			check_and_delete(file_name)
-
-		with open('uploaded_files_to_S3.txt', 'w') as f:
-			f.writelines([file + '\n' for file in uploaded_files_to_S3])
 
 		#######################################################
 
