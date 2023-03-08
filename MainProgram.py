7b26cd641e96be0ad7608b77aaeabf7923354406
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-03-08 14:38:51 +0100

    Update MainProgram.py

--- MainProgram.py
+++ MainProgram.py
@@ -105,7 +105,7 @@ print()
 
 base_url = "https://raw.githubusercontent.com/ajinkya-kulkarni/MPINAT-LSM-Application/main/"
 
-file_names = ["LSM_StreamlitApp.py", "modules.py", "requirements.txt"]
+file_names = ["LSM_StreamlitApp.py", "modules.py", "requirements.txt", "Status_Check.ipynb"]
 
 for file_name in file_names:
 
