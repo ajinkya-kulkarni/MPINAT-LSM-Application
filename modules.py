267a1eb7486cd7263f17d6a502c6ac675e965c3b
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-03-08 11:24:16 +0100

    Added module to create an overview file

--- modules.py
+++ modules.py
@@ -31,6 +31,7 @@ from botocore.exceptions import ClientError
 
 import urllib.request
 import json
+import csv
 from datetime import datetime
 
 import caosdb as db
@@ -181,3 +182,213 @@ def check_last_commit(mode=None):
 		raise Exception(f"Application has been recently updated by the Admin(s). Please wait for 10 more minutes and try again.")
 
 #######################################################################################
+
+def make_LSM_Overview_file():
+
+	# remove LSM_Overview.csv and LSM_Overview.json files from the current working directory
+	if os.path.isfile('LSM_Overview.csv'):
+		os.remove('LSM_Overview.csv')
+	if os.path.isfile('LSM_Overview.json'):
+		os.remove('LSM_Overview.json')
+
+	##############################################
+
+	# Attempt to connect to the Linkahead database without using a proxy server
+	try:
+		db.configure_connection(
+			url = LINKAHEAD_URL,
+			password_method = "plain",
+			ssl_insecure = True, # remove after naming server
+			username = LINKAHEAD_USERNAME,
+			password = LINKAHEAD_PASSWORD,
+			timeout = 1000
+		)
+
+	# If connection fails, try again using a proxy server
+	except:
+		try:
+			db.configure_connection(
+				url = LINKAHEAD_URL,
+				password_method = "plain",
+				ssl_insecure = True, # remove after naming server
+				username = LINKAHEAD_USERNAME,
+				password = LINKAHEAD_PASSWORD,
+				https_proxy = UMG_PROXY,
+				timeout = 1000
+			)
+
+		# If connection still fails, raise an exception
+		except:
+			raise Exception('Unsuccessful connection with the Linkahead DB. Contact the admin(s) for help.')
+
+	##############################################
+
+	# Find all LSM scan entries in the database
+
+	which_type_of_scan = 'LSM_SCAN'
+
+	LSM_entries = db.execute_query('FIND RECORD ' + which_type_of_scan)
+
+	##############################################
+
+	global_entries = []
+
+	for single_entry in LSM_entries:
+		
+		SampleID = list(single_entry.get_property_values('Sample'))[0]
+
+		##############################################
+
+		SampleName = list(db.execute_query(f"FIND SAMPLE WITH id = '{SampleID}'", unique=True).get_property_values('name'))[0]
+		
+		if SampleName is None:
+			SampleName = 'None'
+
+		##############################################
+
+		OperatorID = list(single_entry.get_property_values('operator'))[0]
+
+		##############################################
+
+		GivenName = list(db.execute_query(f"FIND PERSON WITH id = '{OperatorID}'", unique=True).get_property_values('given_name'))[0]
+
+		##############################################
+
+		FamilyName = list(db.execute_query(f"FIND PERSON WITH id = '{OperatorID}'", unique=True).get_property_values('family_name'))[0]
+
+		##############################################
+
+		EmailAddress = list(db.execute_query(f"FIND PERSON WITH id = '{OperatorID}'", unique=True).get_property_values('email_address'))[0]
+
+		##############################################
+
+		Date = list(single_entry.get_property_values('date'))[0]
+
+		##############################################
+
+		DeltaPixelXY = list(single_entry.get_property_values('delta_pixel_xy'))[0]
+
+		##############################################
+
+		DeltaPixelZ = list(single_entry.get_property_values('delta_pixel_z'))[0]
+
+		##############################################
+
+		NumberOfChannels = list(single_entry.get_property_values('number_of_channels'))[0]
+
+		##############################################
+
+		wavelengths_only = []
+
+		for single_filter in list(single_entry.get_property_values('filters')[0]):
+			wavelengths_only.append((list(db.execute_query(f"FIND Wavelengths WITH id = '{single_filter}'", unique=True).get_property_values('name'))[0]))
+
+		str_wavelengths = ", ".join(wavelengths_only)
+
+		##############################################
+
+		if len(wavelengths_only) != NumberOfChannels:
+
+			raise Exception('Number of channels is not equal to number of wavelengths isolated')
+
+		##############################################
+
+		IlluminationLeft = str(list(single_entry.get_property_values('illumination_left'))[0])
+
+		##############################################
+
+		IlluminationRight = str(list(single_entry.get_property_values('illumination_right'))[0])
+
+		##############################################
+
+		Apertures = list(single_entry.get_property_values('apertures')[0])
+
+		Apertures = ", ".join(str(x) for x in Apertures)
+
+		##############################################
+
+		ExposureTimes = list(single_entry.get_property_values('exposure_times')[0])
+
+		ExposureTimes = ", ".join(str(x) for x in ExposureTimes)
+
+		##############################################
+
+		Objective = list(single_entry.get_property_values('objective'))[0]
+
+		##############################################
+
+		Zoom = list(single_entry.get_property_values('zoom'))[0]
+
+		##############################################
+
+		SheetWidth = list(single_entry.get_property_values('sheet_width'))[0]
+
+		##############################################
+
+		AdditionalComments = list(single_entry.get_property_values('additional_comments'))[0]
+
+		if len(AdditionalComments) == 0:
+			AdditionalComments = 'None'
+
+		##############################################
+
+		# create a dictionary to store the properties
+		single_entry_data = {
+			"Scan Type": which_type_of_scan,
+			"Sample ID": str(SampleID),
+			"Sample Name": SampleName,
+			"Operator ID": str(OperatorID),
+			"Operator Given Name": GivenName,
+			"Operator Family Name": FamilyName,
+			"Operator Email Address": EmailAddress,
+			"Date": Date,
+			"Delta Pixel XY": str(DeltaPixelXY),
+			"Delta Pixel Z": str(DeltaPixelZ),
+			"Number of Channels": str(NumberOfChannels),
+			"Wavelengths": str_wavelengths,
+			"Apertures": Apertures,
+			"Exposure Times": ExposureTimes,
+			"Objective": Objective,
+			"Zoom": Zoom,
+			"Sheet Width": str(SheetWidth),
+			"Additional Comments": AdditionalComments
+			}
+
+		##############################################
+
+		global_entries.append(single_entry_data)
+
+		##############################################
+
+	# create the JSON file
+	with open('LSM_Overview.json', 'w') as outfile:
+		json.dump(global_entries, outfile)
+
+	# read the JSON file
+	with open('LSM_Overview.json', 'r') as f:
+		data = json.load(f)
+
+	# extract the headers
+	headers = data[0].keys()
+
+	# get the current timestamp
+	timestamp = datetime.now().strftime('%d %B %Y at %H:%M hrs')
+
+	# write the data to a CSV file
+	with open('LSM_Overview.csv', 'w', newline='') as f:
+		writer = csv.writer(f)
+		
+		# write the 'Created On' row
+		writer.writerow(['Created on', timestamp])
+		
+		# write an empty row
+		writer.writerow([])
+		
+		# write the header row
+		writer.writerow(headers)
+		
+		# write the data rows
+		for entry in data:
+			writer.writerow(entry.values())
+
+	print('LSM_Overview.csv file successfully created!')
